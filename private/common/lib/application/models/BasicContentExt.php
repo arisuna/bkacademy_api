2bcad2179f106426e6203dcf2e22c10a7bcd9e24
Vu Hoang Duc <vu.hoangduc@equusoft.com>
2023-10-22 17:19:53 +0700

    132

---
+++ private/common/lib/application/models/BasicContentExt.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SMXD\Application\Models;
+
+use Phalcon\Mvc\Model\Behavior\SoftDelete;
+use SMXD\Application\Lib\Helpers;
+use Phalcon\Security\Random;
+use Phalcon\Http\Request;
+use Phalcon\Mvc\Model\Relation;
+use SMXD\Application\Lib\ModelHelper;
+use SMXD\Application\Traits\ModelTraits;
+
+class BasicContentExt extends BasicContent
+{
+
+    use ModelTraits;
+
+	/** status archived */
+	const STATUS_ARCHIVED = -1;
+	/** status active */
+	const STATUS_ACTIVE = 1;
+	/** status draft */
+	const STATUS_DRAFT = 0;
+	
+	/**
+	 * [initialize description]
+	 * @return [type] [description]
+	 */
+	public function initialize(){
+		parent::initialize();
+		$this->addBehavior(new \Phalcon\Mvc\Model\Behavior\Timestampable(
+            array(
+                'beforeValidationOnCreate' => array(
+                    'field' => array(
+                        'created_at', 'updated_at'
+                    ),
+                    'format' => 'Y-m-d H:i:s'
+                ),
+                'beforeValidationOnUpdate' => array(
+                    'field' => 'updated_at',
+                    'format' => 'Y-m-d H:i:s'
+                )
+            )
+        ));
+
+	}
+
+
+
+    /**
+     * @param array $custom
+     */
+    public function setData( $custom = []){
+
+         ModelHelper::__setData($this, $custom);
+        /****** YOUR CODE ***/
+
+
+        /****** END YOUR CODE **/
+    }
+}
