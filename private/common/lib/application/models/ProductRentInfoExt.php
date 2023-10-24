9329d957552242a7d54a70e0f1b1fadff93db9e0
Admin <thuy@relotalent.com>
2023-10-24 16:43:52 +0700

    add product rent and sale info

---
+++ private/common/lib/application/models/ProductRentInfoExt.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SMXD\Application\Models;
+
+use Phalcon\Mvc\Model\Behavior\SoftDelete;
+use Phalcon\Validation;
+use SMXD\Application\Lib\Helpers;
+use Phalcon\Security\Random;
+use Phalcon\Http\Request;
+use Phalcon\Mvc\Model\Relation;
+use SMXD\Application\Lib\ModelHelper;
+use SMXD\Application\Traits\ModelTraits;
+
+class ProductRentInfoExt extends ProductRentInfo
+{
+
+    use ModelTraits;
+    const LIMIT_PER_PAGE = 50;
+	
+	/**
+	 * [initialize description]
+	 * @return [type] [description]
+	 */
+	public function initialize(){
+		parent::initialize();
+		$this->addBehavior(new \Phalcon\Mvc\Model\Behavior\Timestampable(
+            array(
+                'beforeValidationOnCreate' => array(
+                    'field' => array(
+                        'created_at', 'updated_at'
+                    ),
+                    'format' => 'Y-m-d H:i:s'
+                ),
+                'beforeValidationOnUpdate' => array(
+                    'field' => 'updated_at',
+                    'format' => 'Y-m-d H:i:s'
+                )
+            )
+        ));
+
+        $this->belongsTo('uuid', '\SMXD\Application\Models\ProductExt', 'uuid', [
+            'alias' => 'Product'
+        ]);
+	}
+
+    /**
+     * @param array $custom
+     */
+    public function setData( $custom = []){
+
+         ModelHelper::__setData($this, $custom);
+        /****** YOUR CODE ***/
+
+
+        /****** END YOUR CODE **/
+    }
+}
