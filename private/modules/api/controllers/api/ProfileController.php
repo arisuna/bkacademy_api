8f90e7a72cff54fdea1e27cc3e6ce2dbd227b8a0
tuandev <tuan@relotalent.com>
2023-11-08 16:09:11 +0700

    154

--- private/modules/api/controllers/api/ProfileController.php
+++ private/modules/api/controllers/api/ProfileController.php
@@ -6,13 +6,13 @@ use SMXD\Api\Controllers\ModuleApiController;
 use SMXD\Api\Models\BusinessOrder;
 use SMXD\Api\Models\User;
 use SMXD\Api\Models\ModuleModel;
+use SMXD\Application\Lib\Helpers;
 
 /**
  * Concrete implementation of App module controller
  *
  * @RoutePrefix("/api")
  */
-
 class ProfileController extends BaseController
 {
     public function indexAction()
@@ -32,7 +32,8 @@ class ProfileController extends BaseController
     }
 
 
-    public function getListOrdersAction(){
+    public function getListOrdersAction()
+    {
         $this->view->disable();
         $this->checkAjaxPutGet();
 
@@ -43,11 +44,11 @@ class ProfileController extends BaseController
             'bind' => [
                 'creator_end_user_id' => $profile->getId()
             ],
-            'order'=> 'created_at desc'
+            'order' => 'created_at desc'
         ]);
 
         $data = [];
-        foreach ($businessOrders as $businessOrder){
+        foreach ($businessOrders as $businessOrder) {
             $data[] = $businessOrder->parsedDataToArray();
         }
 
@@ -55,6 +56,51 @@ class ProfileController extends BaseController
             'success' => true,
             'data' => $data
         ];
+
+        $this->response->setJsonContent($result);
+        return $this->response->send();
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function updateAction()
+    {
+        $this->view->disable();
+        $this->checkAjaxPut();
+
+        $result = [
+            'success' => false,
+            'message' => 'PROFILE_SAVE_FAIL_TEXT',
+        ];
+
+        $dataInput = $this->request->getJsonRawBody(true);
+        $user_uuid = isset($dataInput['uuid']) && $dataInput['uuid'] != '' ? $dataInput['uuid'] : "";
+        $firstname = isset($dataInput['firstname']) && $dataInput['firstname'] != '' ? $dataInput['firstname'] : "";
+        $lastname = isset($dataInput['lastname']) && $dataInput['lastname'] != '' ? $dataInput['lastname'] : "";
+        $email = isset($dataInput['email']) && $dataInput['email'] != '' ? $dataInput['email'] : "";
+        $birthdate = isset($dataInput['birthdate']) && $dataInput['birthdate'] != '' ? $dataInput['birthdate'] : "";
+
+        if ($user_uuid != '' && $firstname && $lastname) {
+            $user = ModuleModel::$user;
+            if ($user->getUuid() == $user_uuid) {
+                $user->setBirthdate($birthdate);
+                $user->setEmail($email);
+                $user->setFirstname($firstname);
+                $user->setLastname($lastname);
+
+                $modelResult = $user->__quickUpdate();
+
+                if ($modelResult instanceof User) {
+                    $result = [
+                        'success' => true,
+                        'message' => 'USER_PROFILE_SAVE_SUCCESS_TEXT',
+                        'data' => $modelResult,
+                    ];
+                }
+            }
+        }
 
         $this->response->setJsonContent($result);
         return $this->response->send();
