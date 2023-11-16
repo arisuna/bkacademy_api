255b3a2030f8d1725158f52aec605ee57f756b55
tuandev <tuan@relotalent.com>
2023-11-16 11:58:02 +0700

    fix get user

--- private/modules/api/models/UserProfile.php
+++ private/modules/api/models/UserProfile.php
@@ -2,17 +2,30 @@
 
 namespace SMXD\Api\Models;
 
+use SMXD\Application\Lib\ModelHelper;
 use SMXD\Application\Models\UserExt;
 use SMXD\Api\Models\ModuleModel;
 use SMXD\Api\Module;
 
-class User extends UserExt {
+class User extends UserExt
+{
     /**
      * [initialize description]
      * @return [type] [description]
      */
-    public function initialize(){
+    public function initialize()
+    {
         parent::initialize();
     }
 
+    public function getParsedArray($columns = NULL)
+    {
+        $array = $this->toArray();
+        $array['isAdmin'] = $this->isAdmin();
+
+        unset($array['id']);
+        unset($array['aws_cognito_uuid']);
+
+        return $array;
+    }
 }
