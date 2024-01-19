a800e0b0a039d41d8b45ec9bdd46cea3b104c675
Admin <thuy@relotalent.com>
2024-01-19 10:30:43 +0700

    progress report

--- private/modules/app/controllers/api/ReportController.php
+++ private/modules/app/controllers/api/ReportController.php
@@ -77,4 +77,54 @@ class ReportController extends BaseController
         $this->response->setJsonContent($result);
         return $this->response->send();
     }
+
+    /**
+     * @Route("/constant", paths={module="backend"}, methods={"GET"}, name="backend-constant-index")
+     */
+    public function getReportProgressAction()
+    {
+    	$this->view->disable();
+        $this->checkAcl(AclHelper::ACTION_INDEX, AclHelper::CONTROLLER_LESSON);
+        $this->checkAjaxPutGet();
+        $params = [];
+        $params['limit'] = Helpers::__getRequestValue('limit');
+        $orders = Helpers::__getRequestValue('orders');
+        $ordersConfig = Helpers::__getApiOrderConfig($orders);
+        $params['page'] = Helpers::__getRequestValue('page');
+        $params['search'] = Helpers::__getRequestValue('query');
+        $params['week'] = Helpers::__getRequestValue('week');
+        $params['date'] = Helpers::__getRequestValue('date');
+        $params['is_main_score'] = Helpers::YES;
+
+        if (is_object($params['date'])) {
+            $params['date'] = (array)$params['date'];
+        }
+        $grades = Helpers::__getRequestValue('grades');
+        if (is_array($grades) && count($grades) > 0) {
+            foreach ($grades as $grade) {
+                $params['grades'][] = $grade->id;
+            }
+        }
+        $weeks = Helpers::__getRequestValue('weeks');
+        if (is_array($weeks) && count($weeks) > 0) {
+            foreach ($weeks as $week) {
+                $params['weeks'][] = $week->id;
+            }
+        }
+        $classrooms = Helpers::__getRequestValue('classrooms');
+        if (is_array($classrooms) && count($classrooms) > 0) {
+            foreach ($classrooms as $classroom) {
+                $params['classrooms'][] = $classroom->id;
+            }
+        }
+        $lesson_types = Helpers::__getRequestValue('lesson_types');
+        if (is_array($lesson_types) && count($lesson_types) > 0) {
+            foreach ($lesson_types as $lesson_type) {
+                $params['lesson_types'][] = $lesson_type->id;
+            }
+        }
+        $result = StudentScore::__findWithFiltersForProgress($params, $ordersConfig);
+        $this->response->setJsonContent($result);
+        return $this->response->send();
+    }
 }
