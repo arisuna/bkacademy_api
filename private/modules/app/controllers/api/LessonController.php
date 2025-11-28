874a50d5c081024790c1d03bf0e12863d35c1f73
Admin <thuy@relotalent.com>
2025-11-28 22:44:12 +0700

    fix bug

--- private/modules/app/controllers/api/LessonController.php
+++ private/modules/app/controllers/api/LessonController.php
@@ -939,8 +939,6 @@ class LessonController extends BaseController
                         "category_id" => $category->getId()
                     ]
                 ]);
-                echo "".json_encode($data)." is home category ". json_encode($lesson_category->getIsHomeCategory())." category id".$category->getId();
-                echo "".json_encode($student_score->toArray())." check instance ".json_encode($student_score instanceof StudentCategoryScore);
                 if($student_score instanceof StudentCategoryScore){
                     if($lesson_category->getIsHomeCategory() == Helpers::NO){
                         $dataArray['categories'][$student_score->getCategoryId()] = $student_score->toArray();
