31d54d4bacebc56f466c986bec5de24906744157
Admin <thuy@relotalent.com>
2025-11-28 10:12:27 +0700

    fix list lesson

--- private/common/lib/application/models/User.php
+++ private/common/lib/application/models/User.php
@@ -990,26 +990,14 @@ class User extends \Phalcon\Mvc\Model
         $this->setSource("user");
     }
 
-    /**
-     * Allows to query a set of records that match the specified conditions
-     *
-     * @param mixed $parameters
-     * @return User[]|User|\Phalcon\Mvc\Model\ResultSetInterface
-     */
-    public static function find($parameters = null): \Phalcon\Mvc\Model\ResultsetInterface
+    public static function find($parameters = null)
     {
         return parent::find($parameters);
     }
 
-    /**
-     * Allows to query the first record that match the specified conditions
-     *
-     * @param mixed $parameters
-     * @return User|\Phalcon\Mvc\Model\ResultInterface
-     */
-    public static function findFirst($parameters = null): \Phalcon\Mvc\ModelInterface
+    public static function findFirst($parameters = null)
     {
-        return parent::findFirst($parameters)?: null;
+        return parent::findFirst($parameters);
     }
 
     protected $source = "user";
